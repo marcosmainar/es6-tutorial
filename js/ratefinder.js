30fd691ca1aa3fe6dbb05504a03bddfc0e050464
Marcos Mainar <marcosmainar@gmail.com>
2017-01-01 16:45:45 +0000

    Creating a native Promise

--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,10 +1,20 @@
 let url = "rates.json";
+
+import * as service from './rate-service-mock';
     
-fetch(url)
-    .then(response => response.json())
+service.findAll()
     .then(rates => {
-      let html = '';
-      rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
-      document.getElementById("rates").innerHTML = html;
+        let html = '';
+        rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
+        document.getElementById("rates").innerHTML = html;
     })
     .catch(e => console.log(e));
+    
+// fetch(url)
+//     .then(response => response.json())
+//     .then(rates => {
+//       let html = '';
+//       rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
+//       document.getElementById("rates").innerHTML = html;
+//     })
+//     .catch(e => console.log(e));
